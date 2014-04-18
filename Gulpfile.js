ba57a2b5a366993773a3024c49a6be313477b2c3
Imre Fazekas <imre.fazekas@icloud.com>
2014-04-18 19:31:20 +0200

    switch to gulp

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = global.gulp = require('gulp'),
+	plugins = global.plugins = require("gulp-load-plugins")( { scope: ['devDependencies'] } );;
+
+gulp.task( 'jshint', function(callback) {
+	return gulp.src( 'knockout.mapper.js' )
+		.pipe( global.plugins.jshint() )
+		.pipe( global.plugins.jshint.reporter('default' ));
+} );
+
+gulp.task( 'uglify', function(callback) {
+	return gulp.src( 'knockout.mapper.js' )
+		.pipe( global.plugins.rename( 'knockout.mapper.min.js') )
+		.pipe( global.plugins.uglify( {outSourceMap: true} ) )
+		.pipe( gulp.dest('./') );
+} );
+
+gulp.task( 'default', [ 'jshint', 'uglify' ] );
